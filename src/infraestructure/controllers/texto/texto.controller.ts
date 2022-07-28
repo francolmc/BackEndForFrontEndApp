3e8d87b00bf39df607294fb53df5c2a1fbdbb2af
francolmc <franco.morales@outlook.com>
2022-07-28 18:37:49 -0400

    feta: nuevo endpoint

--- src/infraestructure/controllers/texto/texto.controller.ts
+++ src/infraestructure/controllers/texto/texto.controller.ts
@@ -3,14 +3,15 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('/api/footer/texto')
 export class TextoController {
   @Get()
-  public async getAllPost(): Promise<string> {
+  public getAllPost(): { message: string } {
     const min = Math.ceil(1);
-    const max = Math.floor(4);
+    const max = Math.floor(5);
     const message = Math.floor(Math.random() * (max - min + 1)) + 1;
-    if (message === 1) return 'Conectado al 100%';
-    if (message === 2) return 'Tu sistema es OK';
-    if (message === 3) return 'Mucho exito (2022)';
-    if (message === 4) return 'Has logrado tener una conexion con esta API';
-    return 'Todos los sercicios en linea!!!';
+    if (message === 1) return { message: 'Conectado al 100%' };
+    if (message === 2) return { message: 'Tu sistema es OK' };
+    if (message === 3) return { message: 'Mucho exito (2022)' };
+    if (message === 4)
+      return { message: 'Has logrado tener una conexion con esta API' };
+    return { message: 'Todos los sercicios en linea!!!' };
   }
 }
